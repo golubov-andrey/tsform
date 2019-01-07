c5153f9976cdbe9b64ec2a2d3d41b2c47fd859dd
Golubov Andrey <golubov.andrey.pavlovich@gmail.com>
2019-01-07 21:10:44 +0400

    Properties value and type is abstract of class PrimitiveField

--- src/field/index.ts
+++ src/field/index.ts
@@ -1,9 +1,9 @@
-export type PrimitiveDataTypeKey = 'Int'|'Float'|'Str'|'Bool'|'Nil'|'Datetime';
+export type PrimitiveDataTypeKey = 'Any'|'Int'|'Float'|'Str'|'Bool'|'Nil'|'Datetime';
 export type PrimitiveDataType = number|string|boolean|null|Date;
 
 export abstract class PrimitiveField<Type extends PrimitiveDataType> {
-  public readonly type: PrimitiveDataType;
-  public readonly value: Type;
+  public abstract readonly type: PrimitiveDataType;
+  public abstract readonly value: Type;
   public toPrimitive(): Type {
     return this.value;
   }
