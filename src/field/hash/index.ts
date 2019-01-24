e029cf389c0274b94c07610c4a1040a966a11223
Golubov Andrey <a.golubov@taxi-master.com>
2019-01-24 19:06:01 +0400

    Доработки типов для Hash

--- src/field/hash/index.ts
+++ src/field/hash/index.ts
@@ -2,7 +2,7 @@ import { Primitive as PrimitiveField } from '../index';
 import * as Primitive from '../primitive';
 
 export type TypeKey = 'Hash';
-export type Type<T> = { [K in keyof T]: T[K] extends PrimitiveField | IType<T> ? T[K] : undefined };
+export type Type<T> = { [K in keyof T]: T[K] extends PrimitiveField | IType<T> ? T[K] : never };
 export interface IType<T> extends Field<T> { }
 
 export class Field<V> extends Primitive.Field<Type<V>, TypeKey> {
@@ -11,6 +11,11 @@ export class Field<V> extends Primitive.Field<Type<V>, TypeKey> {
   constructor(
       readonly value: Type<V>,
   ) { super(); }
+
+  // tslint:disable-next-line:max-line-length
+  public extract(): { [K in keyof V]: V[K] extends PrimitiveField ? V[K]['value'] : V[K] extends IType<V> ? ReturnType<V[K]['extract']> : never } {
+    return {} as any;
+  }
 }
 
 export default function hash<T>(x: Type<T>) {
