1baa0cc66c30b7335e262ac8f80c4b5aad792f8a
Golubov Andrey <a.golubov@taxi-master.com>
2019-01-25 15:24:05 +0400

    Documentation

--- src/field/hash/index.ts
+++ src/field/hash/index.ts
@@ -1,20 +1,46 @@
-import f, { Primitive as PrimitiveField } from '../index';
-import * as Primitive from '../primitive';
+import f, { Primitive } from '../index';
+import { Field } from '../primitive';
 
 export type TypeKey = 'Hash';
 // tslint:disable-next-line:max-line-length
-export type Type<T> = { [K in keyof T]: T[K] extends PrimitiveField ? T[K] : T[K] extends IType<any> ? IType<T[K]['value']> : never };
+export type Type<T> = { [K in keyof T]: T[K] extends Primitive ? T[K] : T[K] extends IType<any> ? IType<T[K]['value']> : never };
 export interface IType<T> extends Hash<T> { }
 // tslint:disable-next-line:max-line-length
-export type Extract<V> =  { [K in keyof V]: V[K] extends PrimitiveField ? V[K]['value'] : V[K] extends IType<any> ? ReturnType<V[K]['extract']> : never };
+export type Extract<V> =  { [K in keyof V]: V[K] extends Primitive ? V[K]['value'] : V[K] extends IType<any> ? ReturnType<V[K]['extract']> : never };
 
-export class Hash<V> extends Primitive.Field<Type<V>, TypeKey> {
+/**
+ * Complex `HashField`
+ */
+export class Hash<V> extends Field<Type<V>, TypeKey> {
+  /**
+   * Typekey of `HashField`
+   */
   public static Type: TypeKey = 'Hash';
+  /**
+   * TypeKey of `HashField`
+   */
   public readonly type: TypeKey = 'Hash';
+  /**
+   * Returned complex `HashField`
+   * @param value `imutable` value of `HashField`
+   */
   constructor(
       readonly value: Type<V>,
   ) { super(); }
-
+  /**
+   * Returned new `HashField` with values from `primitive`
+   * ```
+   * import f from 'tsform/field';
+   *
+   * const entity = f.hash({
+   *   someStr: f.str(),
+   * }).from({
+   *   someStr: 'some text',
+   * });
+   * /// => f.hash({ someStr: f.str('some text') })
+   * ```
+   * @param primitive `any` record
+   */
   public from(primitive: any): IType<V> {
     return hash((Object.keys(this.value) as Array<keyof Type<V>>).reduce((acc, key) => {
       const primitiveValue = primitive[key];
@@ -33,7 +59,17 @@ export class Hash<V> extends Primitive.Field<Type<V>, TypeKey> {
       return acc;
     }, {} as any));
   }
-
+  /**
+   * Returned primitive `record` from `HashField`
+   * ```
+   * import f from 'tsform/field';
+   *
+   * const primitive = f.hash({
+   *  someStr: f.text('some long text'),
+   * }).extract();
+   * /// => { someStr: 'some long text' }
+   * ```
+   */
   public extract(): Extract<V> {
     return (Object.keys(this.value) as Array<keyof Type<V>>).reduce((acc, key) => {
       switch (this.value[key].type) {
@@ -45,6 +81,20 @@ export class Hash<V> extends Primitive.Field<Type<V>, TypeKey> {
   }
 }
 
+/**
+ * Returned new complex `HashField` with record of primitives or other complexes
+ * ```
+ * import f from 'tsform/field';
+ *
+ * const entity = f.hash({
+ *   text: f.text('Some long text'),
+ *   ping: f.hash({
+ *     pong: f.int(1),
+ *   }),
+ * });
+ * ```
+ * @param x `any` record
+ */
 export default function hash<T>(x: Type<T>) {
   return new Hash(x);
 }
